421bec505965bafa3fbeac33d24533b3a3aae751
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-11-30 14:57:29 +0100

    Simple JS Operations

--- index.js
+++ index.js
@@ -7,41 +7,69 @@
 // 	If you add a number to a string, the number will be treated as string, and concatenated.
 
 // 	JavaScript objects are containers for named values (called properties) and methods.
-var loginDetails = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor:"blue"
-};
-var cars = ["Lambo", "Volvo", "BMW"];
-var onOrOff = true;
-var teacherName = "Mr Fotang";
+// var loginDetails = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     age: 50,
+//     eyeColor:"blue"
+// };
+// var cars = ["Lambo", "Volvo", "BMW"];
+// var onOrOff = true;
+// var teacherName = "Mr Fotang";
 
-//
-// errors practicing this JS objects topic
-//function fullName(firstName, lastName) {
-//    return (loginDetails.firstName) + "" + (loginDetails.lastName);
-//}
+/*
+errors practicing this JS objects topic
+function fullName(firstName, lastName) {
+    return (loginDetails.firstName) + "" + (loginDetails.lastName);
+}
+*/
 
-// below is a commment 
-document.getElementById("count-el").innerText = "5";
+// let firstBatch = 5
+// let secondBatch = 7
 
-// logging count to be zero ON THE CONSOLE
-let count = 0;
-console.log(count);
+// let batches = firstBatch + secondBatch
 
-// access object properties
-loginDetails.firstName;
-loginDetails["lastName"];
+// console.log(batches)
+
+
+// humanDogRatio matter
+// let myAge = 22
+// let humanDogRatio = 7
+// let myDogAge = myAge * humanDogRatio
+// console.log(myDogAge)
+
+
+// bonusPoints Matter
+let bonusPoints = 50
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints - 75
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints + 45
+console.log(bonusPoints)
+
+// // below is a commment 
+// document.getElementById("count-el").innerText = "5";
+
+// // logging count to be zero ON THE CONSOLE
+// let count = 0;
+// console.log(count);
+
+// // access object properties
+// loginDetails.firstName;
+// loginDetails["lastName"];
 
 // access object methods
-loginDetails.fullName();
+// loginDetails.fullName();
 
 // use typeof to return variable type
-typeof loginDetails;
-typeof cars;
+// typeof loginDetails;
+// typeof cars;
 
 // function to change fahrenhite to celsius
-function toCelsius(fahrenhite) {
-    return (5/9) * (fahrenhite-32);
-}
+// function toCelsius(fahrenhite) {
+//     return (5/9) * (fahrenhite-32);
+// }
