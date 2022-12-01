b5ac071346e48a9c2b05b6a4534fcd750a9ef7a1
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-12-01 04:15:17 +0100

    Record of people entered at every round

--- Passanger Counter/passangerCounter.js
+++ Passanger Counter/passangerCounter.js
@@ -1,12 +1,17 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
 let count = 0
 
 // increment function
 function increment() {
     count = count + 1
-    document.getElementById("count-el").innerHTML = count
+    countEl.textContent = count
 }
 
 // save function
 function save() {
+    let countstr = count + " - "
+    //textContent is used rather than innerText because, it shows even elements that are non human readable, like spaces
+    saveEl.innerText = saveEl.textContent + countstr
     console.log(count)
 }
