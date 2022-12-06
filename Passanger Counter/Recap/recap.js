63c37d34b6215d78f7c65c5aa863ec7dd5b2d96a
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-12-06 14:39:59 +0100

    nike error recap

---
+++ Passanger Counter/Recap/recap.js
@@ -0,0 +1,39 @@
+function purchaseError() {
+    document.getElementById("error").innerHTML= "Something went wrong, please try again"
+}
+
+// increment decrement
+// let myPoints = 3
+
+// function add3Points() {
+// //    myPoints = myPoints + 1
+//       myPoints += 3
+// }
+
+// function remove1Point() {
+// //    myPoints = myPoints - 1
+//       myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// console.log(myPoints)
+// concatenate strings in a function
+// let noms = "Linda"
+// let greetings = "Hi there"
+// function greetLinda() {
+//     console.log(greetings + " " + noms + "!")
+// }
+
+// greetLinda()
+
+
+// concatenate variables
+// let firstName = "nso"
+// let lastName = "roosvelt"
+
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
