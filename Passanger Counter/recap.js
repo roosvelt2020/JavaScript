bb4277eb4af1e2bbc62b23b3f92d1aba314fc9d6
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-12-06 13:02:40 +0100

    concatenation recap

---
+++ Passanger Counter/recap.js
@@ -0,0 +1,18 @@
+
+
+// concatenate strings in a function
+// let noms = "Linda"
+// let greetings = "Hi there"
+// function greetLinda() {
+//     console.log(greetings + " " + noms + "!")
+// }
+
+// greetLinda()
+
+
+// concatenate variables
+// let firstName = "nso"
+// let lastName = "roosvelt"
+
+// let fullName = firstName + " " + lastName
+// console.log(fullName)
