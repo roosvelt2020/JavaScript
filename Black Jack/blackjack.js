73b6fef31d71ea6aa580e8d8ad30b2ecba7b2537
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-12-08 03:39:13 +0100

    display want to draw a new card question

--- Black Jack/blackjack.js
+++ Black Jack/blackjack.js
@@ -1,19 +1,21 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 4
+let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
-let sum = firstCard + secondCard
 let message = ""
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?"
-} else if (sum === 21) {
-    message = "Wohoo! You've got blackjack"
-    hasBlackJack = true
-} else {
-    message = "You're out of the game! "
-    isAlive = false
-}
+let messageEl = document.getElementById("message-el")
 
-// CASH OUT:
-console.log(hasBlackJack)
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got blackjack"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game! "
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
